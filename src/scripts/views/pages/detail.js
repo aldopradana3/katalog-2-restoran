9c6a2d16713099d30b01c9bd3b6bc574708d4c2d
Aldo Pradana Ariando <87022323+aldopradana3@users.noreply.github.com>
2023-05-26 19:27:48 +0700

    Add files via upload

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,34 @@
+import UrlParser from '../../routes/url-parser';
+import RestaurantSource from '../../data/source';
+import { createRestaurantDetailTemplate } from '../templates/template-creator';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
+
+const Detail = {
+  async render() {
+    return `
+        <h2 class="explore-title">Detail Page</h2>
+        <div id="restaurant" class="restaurant"></div>
+        <div id="likeButtonContainer"></div>
+      `;
+  },
+
+  async afterRender() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const restaurant = await RestaurantSource.detailRestaurant(url.id);
+    const restaurantContainer = document.querySelector('#restaurant');
+    restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant);
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: restaurant.id,
+        city: restaurant.city,
+        name: restaurant.name,
+        rating: restaurant.rating,
+        pictureId: restaurant.pictureId,
+      },
+    });
+  },
+};
+
+export default Detail;
---
+++ src/scripts/views/pages/favorite.js
@@ -0,0 +1,24 @@
+import FavoriteRestaurantIdb from '../../data/favorite-restaurant-idb';
+import { createRestaurantItemTemplate } from '../templates/template-creator';
+
+const Favorite = {
+  async render() {
+    return `
+    <div class="explore-list">
+      <h1 class="explore-title">Favorite Restaurant</h1>
+    </div>
+    <div id="restaurants" class="restaurants">
+      `;
+  },
+
+  async afterRender() {
+    const restaurants = await FavoriteRestaurantIdb.getAllRestaurants();
+    const restaurantsContainer = document.querySelector('#restaurants');
+
+    restaurants.forEach((restaurant) => {
+      restaurantsContainer.innerHTML += createRestaurantItemTemplate(restaurant);
+    });
+  },
+};
+
+export default Favorite;
